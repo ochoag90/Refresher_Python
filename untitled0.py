372ad062da07778f3efc2a1e696e6af71221fa5a
Gabriel Ochoa <gabrielochoa@Gabriels-MacBook-Pro.local>
2020-07-04 13:20:21 -0400

    firs commit

---
+++ untitled0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  2 19:36:31 2020
+
+@author: gabrielochoa
+"""
+
+
+print('hello\'s, \n\tworld!')
---
+++ untitled1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  3 11:11:47 2020
+
+@author: gabrielochoa
+"""
+
+print('One, \'two\', \"three\", four, \nfive,\nonce, \'I\' caught a fish \'//alive\\\\\'')
